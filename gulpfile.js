17ba02240b1f8d3768a259ecb1600f996ef6a5a4
Zachary Jones <zacharytamas@gmail.com>
2015-10-10 01:51:44 -0400

    Start on site layout. Just playing around at the moment.

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+var merge = require('merge-stream');
+var path = require('path');
+
+function distPath(path) {
+  return 'static/dist/' + path;
+}
+
+var styleTask = function(stylesPath, srcs) {
+  return gulp
+    .src(srcs.map(function(src) {
+      return path.join('static', stylesPath, src); }
+    ))
+    // .pipe($.changed('static/' + stylesPath, {extension: '.css'}))
+    // TODO possibly add CSS minification here
+    .pipe(gulp.dest('static/dist/' + stylesPath))
+    .pipe($.size({title: stylesPath}));
+};
+
+gulp.task('styles', styleTask.bind(gulp, '', ['*.css']));
+
+gulp.task('sass', function() {
+  return gulp
+    .src(['static/*.scss'])
+    .pipe($.sass().on('error', $.sass.logError))
+    .pipe(gulp.dest('static/dist'))
+    .pipe($.size({title: 'sass'}));
+});
+
+gulp.task('copy', function() {
+  var bower = gulp.src([
+    'bower_components/**/*'
+  ]).pipe(gulp.dest(distPath('bower_components')));
+
+  // var elements = gulp.src(['static/elements-*.html'])
+  //   .pipe(gulp.dest(distPath('elements')));
+
+  // var vulcanizeCore = gulp.src(['static/elements-core.html'])
+  //   .pipe($.rename('core.vulcanized.html'))
+  //   .pipe(gulp.dest(distPath('elements')));
+
+  return merge(bower)
+    .pipe($.size({title: 'copy'}));
+});
+
+// Vulcanize imports
+gulp.task('vulcanize', function() {
+  var DEST_DIR = 'static/dist';
+
+  return gulp.src('static/elements-core.html')
+    .pipe($.vulcanize({
+      stripComments: true,
+      inlineCss: true,
+      inlineScripts: true
+    }))
+    .pipe(gulp.dest(DEST_DIR))
+    .pipe($.size({title: 'vulcanize'}));
+});
+
+gulp.task('develop', function() {
+
+  browserSync({
+    notify: false,
+    logPrefix: 'PSK',
+    snippetOptions: {
+      rule: {
+        match: '<span id="browser-sync-binding"></span>',
+        fn: function(snippet) {
+          return snippet;
+        }
+      }
+    },
+    proxy: 'localhost:8080'
+  });
+
+  gulp.watch(['static/*.css'], ['styles']);
+  gulp.watch(['static/*.scss'], ['sass']);
+  gulp.watch(['static/elements-*.html', 'static/elements/**/*'], ['vulcanize']);
+  // gulp.watch(['static/dist/*.css'], browserSync.reload);
+});

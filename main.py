cd84ce50e6f3fb357dd54425e4cc9cfb80ddc72e
Zachary Jones <zacharytamas@gmail.com>
2015-10-11 18:05:12 -0400

    Restructure how this all works to be actually separate pages. It's cleaner.

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import webapp2
+import logging
+from webapp2_extras import routes
+
+from utils import render_to_text
+
+class PageHandler(webapp2.RequestHandler):
+
+  element_name = None
+
+  def get(self):
+    logging.info('here ' + self.element_name)
+    self.response.write(render_to_text('page.html', {
+      'element_name': self.element_name
+    }))
+
+  @classmethod
+  def HandlerFor(cls, element):
+    class stub(PageHandler):
+      element_name = element
+    return stub
+
+
+app = webapp2.WSGIApplication([
+  ('/',               PageHandler.HandlerFor('photo-cover')),
+  ('/staying-there/', PageHandler.HandlerFor('staying-there'))
+], debug=False)

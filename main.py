b33018edfab8847d269c4379dab5362001c34cdd
Zachary Jones <zacharytamas@gmail.com>
2015-10-24 23:13:33 -0400

    First sitting on updating the Spreadsheet with RSVP information.
    Never done this before but it was cool!

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+import json
 import webapp2
 import logging
 from webapp2_extras import routes
 
 from utils import render_to_text
+from spreadsheet_api import SpreadsheetService
 
 class PageHandler(webapp2.RequestHandler):
 
@@ -22,7 +24,40 @@ class PageHandler(webapp2.RequestHandler):
     return stub
 
 
+class RSVPHandler(webapp2.RequestHandler):
+  """Handler for RSVP actions."""
+
+  def guestLookup(self):
+    service = SpreadsheetService()
+    code = self.request.get('code')
+
+    if code:
+      guestInfo = service.guestLookup(code)
+      self.response.write(json.dumps(guestInfo, indent=4))
+
+  def respond(self):
+    service = SpreadsheetService()
+    code = self.request.get('code')
+    attending = self.request.get('attending')
+    headcount = self.request.get("headcount")
+    meal_preference = self.request.get("mealPreference")
+
+    if not all([code, attending, headcount, meal_preference]):
+      logging.error("Invalid request to the respond endpoint...")
+      # TODO Return an InvalidRequest
+      return
+
+    service.RSVP(code,
+                 attending == "true",
+                 headcount,
+                 meal_preference)
+
+    # TODO Return properly
+
 app = webapp2.WSGIApplication([
-  ('/',               PageHandler.HandlerFor('photo-cover')),
-  ('/staying-there/', PageHandler.HandlerFor('staying-there'))
+  webapp2.Route('/',                       PageHandler.HandlerFor('photo-cover')),
+  webapp2.Route('/staying-there/',         PageHandler.HandlerFor('staying-there')),
+  webapp2.Route('/rsvp/api/guest-lookup/', RSVPHandler, handler_method='guestLookup'),
+  # TODO Make respond POST-only
+  webapp2.Route('/rsvp/api/respond/',      RSVPHandler, handler_method='respond'),
 ], debug=False)
--- spreadsheet_api.py
+++ spreadsheet_api.py
@@ -1,3 +1,114 @@
+import json
+import gspread
+import logging
+from oauth2client.client import SignedJwtAssertionCredentials
 
-class Spreadsheet:
-  pass
+# credentials.json file from Google Cloud Console.
+# See: http://gspread.readthedocs.org/en/latest/oauth2.html
+CONFIG = json.load(open('credentials.json'))
+scope = ['https://spreadsheets.google.com/feeds']
+
+credentials = SignedJwtAssertionCredentials(CONFIG['client_email'], CONFIG['private_key'].encode(), scope)
+
+DATA_WORKSHEET_NAME = "Data"
+COL_INFO_NAME_PRIMARY = 1
+
+COL_NAMES = [
+  "primaryName",
+  "secondaryNames",
+  "expectedHeadCount",
+  "brideOrGroom",
+  "category",
+  "addressStreet",
+  "addressCity",
+  "addressState",
+  "addressZipCode",
+  "-",
+  "rsvpCode",
+  "rsvpResponseMethod",
+  "rsvpAttending",
+  "rsvpHeadcount",
+  "rsvpMealPreference",
+]
+
+COL_INDEXES = {}
+COL_LETTERS = {}
+
+# NOTE This just dynamically generates all the column indexes with their
+# names from above. This is easier in case I need to change the schema of
+# my spreadsheet later: I can just enter the new column in the list above
+# in its appropriate place and not have to worry about recalculating all
+# the indexes.
+def magicIndexes():
+  count = 1
+  for name in COL_NAMES:
+    COL_INDEXES[name] = count
+    # This little hack will work as long as we stay under 26 columns
+    COL_LETTERS[name] = chr((count - 1) + ord("A"))
+    count += 1
+magicIndexes()
+
+SHARED_SERVICE = gspread.authorize(credentials)
+
+
+class SpreadsheetService(object):
+
+  spreadsheet_id = CONFIG['spreadsheet_id']  # For privacy.
+
+  service = None
+  spreadsheet = None
+  worksheet = None
+
+  def __init__(self):
+    self.service = SHARED_SERVICE
+    self.__getSpreadsheet()
+
+  def __getSpreadsheet(self):
+    if self.spreadsheet is None or self.worksheet is None:
+      self.spreadsheet = self.service.open_by_key(self.spreadsheet_id)
+      self.worksheet = self.spreadsheet.worksheet(DATA_WORKSHEET_NAME)
+
+  def __findRowForCode(self, code):
+    """Finds the row of the spreadsheet which corresponds to a
+    given RSVP code."""
+
+    try:
+      return self.worksheet.col_values(COL_INDEXES["rsvpCode"]).index(code) + 1
+    except ValueError:
+      return
+
+  def __writeValue(self, row_number, col_name, value):
+    """Convenience method for writing a value to the spreadsheet."""
+    self.worksheet.update_cell(row_number, COL_INDEXES[col_name], value)
+
+  def __getValueFromRow(self, row, col_name):
+    """Just a convenience accessor to get values from
+    certain columns of a row."""
+    return row[COL_INDEXES[col_name] - 1]
+
+  def guestLookup(self, code):
+    """Returns information about a guest based on their invite code."""
+    row_number = self.__findRowForCode(code)
+    row = self.worksheet.row_values(row_number)
+
+    return {
+      "primaryName": self.__getValueFromRow(row, "primaryName")
+    }
+
+  def RSVP(self, code, attending, headcount, meal_preference):
+    row_number = self.__findRowForCode(code)
+
+    if not row_number:
+      raise ValueError("RSVP not found")
+
+    rsvp_range = "%s%d:%s%d" % (COL_LETTERS["rsvpResponseMethod"], row_number,
+                                COL_LETTERS["rsvpMealPreference"], row_number)
+
+    # This quirky method allows us to update them in a batch.
+    selection = self.worksheet.range(rsvp_range)
+    values = ["Online", "YES" if attending else "NO", headcount, meal_preference]
+
+    for cell, value in zip(selection, values):
+      cell.value = value
+
+    self.worksheet.update_cells(selection)
